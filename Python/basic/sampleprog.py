d71fd41d390172de6a7408cee5550997bd52155f
Paul Foster <paul.foster@soartech.com>
2016-07-20 14:18:29 -0400

    add python basic file

---
+++ Python/basic/sampleprog.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# Template for best practices python program
+import argparse
+
+# Input Args
+infile = []
+verbose = []
+
+
+def parse_args():
+    global infile, verbose
+    parser = argparse.ArgumentParser(description="Show a demo program with arguments")
+    parser.add_argument("input",
+                        help="the name of the input file to use",
+                        type=str,
+                        default="a.in")
+    parser.add_argument("-v", "--verbose",
+                        help="Turn on verbose mode.",
+                        action="store_true")
+    args = parser.parse_args()
+    infile = args.input
+    verbose = args.verbose
+
+
+def main():
+    parse_args()
+
+
+
+
+if __name__ == "__main__":
+    main()
